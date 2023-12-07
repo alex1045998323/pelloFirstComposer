3822128fe51ce345c54ac5e4d6ab46cb9cd1a7e9
alex崔 <1045998323@qq.com>
2023-12-07 18:17:20 +0800

    新代码提交

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *|--------------------------------------------------------------------------
+ *| Author     : 1045998323@qq.com
+ *| Class      : ${NAME}
+ *| CreateTime ：17:37
+ *| Notes      : 类说明
+ *|--------------------------------------------------------------------------
+ */
+require "vendor/autoload.php";
+$Test = new MyFirstComposer\Siam\Test();
+$Test->test();
+MyFirstComposer\Study::goLook();
+MyFirstComposer\Study::goWrite();
+MyFirstComposer\Study::goMake();
---
+++ src/Study.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *|--------------------------------------------------------------------------
+ *| Author     : 1045998323@qq.com
+ *| Class      : ${NAME}
+ *| CreateTime ：18:01
+ *| Notes      : 类说明
+ *|--------------------------------------------------------------------------
+ */
+namespace MyFirstComposer;
+class Study {
+    public function __construct() {} // 构造函数为空
+    public static function goLook(){
+        echo "去看--->goLook<br/>";
+    }
+    public static function goWrite(){
+        echo "去写--->goWrite<br/>";
+    }
+    public static function goMake(){
+        echo "去做--->goMake<br/>";
+    }
+}
